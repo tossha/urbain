a27ac872e9b4859c8e1fcb779af4586e15c6aacd
gromov <gromov@playform.com>
2016-10-11 18:55:30 +0300

    First basic class structure skeleton

---
+++ classes.js
@@ -0,0 +1,152 @@
+class Body
+{
+    constructor(visualModel, physicalModel, trajectory, orientation) {
+        this.visualModel   = visualModel;    // class VisualBodyModel
+        this.physicalModel = physicalModel;  // class PhysicalBodyModel
+        this.trajectory    = trajectory;     // class TrajectoryAbstract
+        this.orientation   = orientation;
+
+        this.visualModel.body = this;
+    }
+}
+
+class VisualBodyModel
+{
+    constructor(shape, model, texture) {
+        this.shape   = shape;   // class VisualShapeAbstract
+        this.texture = texture;
+
+        this.body = null;
+
+        // ...
+    }
+}
+
+class VisualShapeAbstract
+{
+    getThreeGeometry() {}
+}
+
+class VisualShapeSphere extends VisualShapeAbstract
+{
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getThreeGeometry() {
+        // @todo implement
+    }
+}
+
+class VisualShapeModel extends VisualShapeAbstract
+{
+    constructor(modelFile) {
+        this.modelFile = modelFile;
+    }
+
+    getThreeGeometry() {
+        // @todo implement
+    }
+}
+
+class PhysicalBodyModel
+{
+    constructor(mu, radius) {
+        this.mu     = mu;     // gravitational parameter
+        this.radius = radius;
+    }
+}
+
+class ReferenceFrame
+{
+    constructor(origin, type) {
+        this.origin = origin || SOLAR_SYSTEM_BARYCENTER; // or maybe just EARTH?
+        this.type = type || RF_TYPE_INERTIAL; // RF_TYPE_INERTIAL или RF_TYPE_ROTATING
+    }
+
+    getMatrixByEpoch(epoch) {
+        // @todo implement
+    }
+
+    getTransformationMatrixByEpoch(epoch, destinationFrame) {
+        if ((this.origin === destinationFrame.origin)
+            && (this.type === destinationFrame.type)
+        ) {
+            return false; // @todo think about identity matrix here
+        }
+
+        // @todo implement
+    }
+}
+
+class StateVector
+{
+    constructor(x, y, z, vx, vy, vz) {
+        this.vector = new Float64Array([
+            x || 0,
+            y || 0,
+            z || 0,
+            vx || 0,
+            vy || 0,
+            vz || 0
+        ]);
+    }
+
+    get position() {
+        return new Vector3(this.vector[0], this.vector[1], this.vector[2]);
+    }
+
+    get velocity() {
+        return new Vector3(this.vector[3], this.vector[4], this.vector[5]);
+    }
+}
+
+class TrajectoryAbstract
+{
+    constructor() {
+        this.referenceFrame = null; // class ReferenceFrame
+    }
+
+    getStateByEpoch(epoch, referenceFrame) {
+        return ZERO_STATE_VECTOR;
+    }
+
+    getPositionByEpoch(epoch, referenceFrame) {
+        return this.getStateByEpoch(epoch, referenceFrame).position;
+    }
+
+    getVelocityByEpoch(epoch, referenceFrame) {
+        return this.getStateByEpoch(epoch, referenceFrame).velocity;
+    }
+}
+
+class TrajectoryStateArray extends TrajectoryAbstract
+{
+    constructor() {
+        this.states = []; // array of [class StateVector, epoch]
+        this.minEpoch = null;
+        this.maxEpoch = null;
+    }
+
+    addState(state, epoch) {
+        this.state.push([state, epoch]);
+
+        if ((this.minEpoch === null)
+            || (epoch < this.minEpoch)
+        ) {
+            this.minEpoch = epoch;
+        }
+
+        if ((this.maxEpoch === null)
+            || (epoch > this.maxEpoch)
+        ) {
+            this.maxEpoch = epoch;
+        }
+    }
+
+    getStateByEpoch(epoch, referenceFrame) {
+        // @todo implement
+    }
+}
+
+const ZERO_STATE_VECTOR = new StateVector();
---
+++ const.js
@@ -0,0 +1,26 @@
+const SOLAR_SYSTEM_BARYCENTER = 0;
+const SUN                     = 10;
+const MERCURY_BARYCENTER = 1;
+const VENUS_BARYCENTER   = 2;
+const EARTH_BARYCENTER   = 3;
+const MARS_BARYCENTER    = 4;
+const JUPITER_BARYCENTER = 5;
+const SATURN_BARYCENTER  = 6;
+const URANUS_BARYCENTER  = 7;
+const NEPTUNE_BARYCENTER = 8;
+const PLUTO_BARYCENTER   = 9;
+const MERCURY = 199;
+const VENUS   = 299;
+const EARTH   = 399;
+const MARS    = 499;
+const JUPITER = 599;
+const SATURN  = 699;
+const URANUS  = 799;
+const NEPTUNE = 899;
+const PLUTO   = 999;
+
+const RF_TYPE_INERTIAL = 0; // reference frame type: inertial
+const RF_TYPE_ROTATING = 1; // reference frame type: rotating with the body
+
+const VM_SHAPE_SPHERE = 1; // visual model shape type: sphere
+const VM_SHAPE_MODEL  = 2; // visual model shape type: custom 3d model
