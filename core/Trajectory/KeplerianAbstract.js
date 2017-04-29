4d9824950b8e59b34dfcae907fe672b448e3e3fb
Anton Gromov <spb@sch239.net>
2017-04-29 18:50:50 +0300

    Reworked keplerian orbits, added precession model

---
+++ core/Trajectory/KeplerianAbstract.js
@@ -0,0 +1,20 @@
+class TrajectoryKeplerianAbstract extends TrajectoryAbstract
+{	
+	getKeplerianObjectByEpoch(epoch) {}
+	
+    constructor(referenceFrame, color) {
+        super(referenceFrame);
+
+        if (color) {
+            this.visualModel = new VisualTrajectoryModelKeplerian(this, color);
+        }
+    }
+
+	isEditable() {
+		return false;
+	}
+	
+	getStateInOwnFrameByEpoch(epoch) {
+        return this.getKeplerianObjectByEpoch(epoch).getStateByEpoch(epoch);
+    }
+}
---
+++ core/Trajectory/KeplerianPrecessing.js
@@ -0,0 +1,24 @@
+class TrajectoryKeplerianPrecessing extends TrajectoryKeplerianBasic
+{
+    constructor(referenceFrame, keplerianObject, r, j2, color) {
+        super(referenceFrame, keplerianObject, color);
+
+        this.keplerianObject = keplerianObject;
+		this.r = r;
+		this.j2 = j2;
+    }
+	
+    getKeplerianObjectByEpoch(epoch) {
+		return this.keplerianObject.addPrecession(this.r, this.j2, epoch);
+	}
+
+    static createFromState(referenceFrame, state, mu, r, j2, epoch, color) {
+        return new TrajectoryKeplerianPrecessing(
+            referenceFrame,
+            KeplerianObject.createFromState(state, mu, epoch),
+			r,
+			j2,
+            color
+        );
+    }
+}
