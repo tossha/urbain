bbca66078cbbffa4ea59de8e1c4652b766c4a3a7
Anton Gromov <tossha.spb@gmail.com>
2017-12-16 19:31:32 +0300

    Loading textures only when needed (#96)

--- src/visual/BodyModel/Basic.js
+++ src/visual/BodyModel/Basic.js
@@ -6,14 +6,28 @@ export default class VisualBodyModelBasic extends VisualBodyModelAbstract
     constructor(shape, color, texturePath) {
         super(shape, color);
 
-        if (texturePath) {
-            let that = this;
+        this.texturePath = texturePath;
+        this.isTextureRequested = false;
+    }
 
+    getMaterial(parameters) {
+        parameters.metalness = 0;
+        parameters.roughness = 1;
+        return new THREE.MeshStandardMaterial(parameters);
+    }
+
+    render(epoch) {
+        super.render(epoch);
+        if (!this.isTextureRequested
+            && this.texturePath
+            && this.shape.getMaxDimension() / this.threeObj.position.length() > sim.raycaster.getPixelAngleSize()
+        ) {
+            this.isTextureRequested = true;
             sim.textureLoader.load(
-                VisualModelAbstract.texturePath + texturePath,
-                function(txt) {
-                    that.threeObj.material.dispose();
-                    that.threeObj.material = that.getMaterial({map: txt});
+                VisualModelAbstract.texturePath + this.texturePath,
+                (txt) => {
+                    this.threeObj.material.dispose();
+                    this.threeObj.material = this.getMaterial({map: txt});
                 },
                 undefined,
                 function(err) {
@@ -21,11 +35,5 @@ export default class VisualBodyModelBasic extends VisualBodyModelAbstract
                 }
             );
         }
-    }
-
-    getMaterial(parameters) {
-        parameters.metalness = 0;
-        parameters.roughness = 1;
-        return new THREE.MeshStandardMaterial(parameters);
     }
 }
--- src/visual/Shape/Abstract.js
+++ src/visual/Shape/Abstract.js
@@ -1,4 +1,6 @@
 export default class VisualShapeAbstract
 {
     getThreeGeometry() {}
+
+    getMaxDimension() {}
 }
