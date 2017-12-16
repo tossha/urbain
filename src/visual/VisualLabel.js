79dbd7fe83942ae33db01a5e7f41a2ac3f603ade
Ironimus <ironimus13@gmail.com>
2017-12-16 20:38:09 +0200

    Added posibility to change text label's scale depending on camera position

--- src/visual/VisualLabel.js
+++ src/visual/VisualLabel.js
@@ -4,23 +4,27 @@ export const DEFAULT_TEXT_SETTINGS = {
     text: '',
     font: 'Arial',
     color: 'white',
-    size: 20
+    size: 20,
+    scalingRange: {
+        from: 30000,
+        to: 400000
+    }
 };
 export default class VisualLabel extends VisualModelAbstract {
     constructor(functionOfEpoch, parameters) {
         super();
-        parameters = Object.assign({}, DEFAULT_TEXT_SETTINGS, parameters);
+        this.parameters = Object.assign({}, DEFAULT_TEXT_SETTINGS, parameters);
         this.functionOfEpoch = functionOfEpoch;
         this.canvas = document.createElement('canvas');
         let context = this.canvas.getContext('2d');
-        this.canvas.height = THREE.Math.ceilPowerOfTwo(parameters.size);
-        context.font = parameters.size + 'px ' + parameters.font;
-        let textWidth = context.measureText(parameters.text).width;
+        this.canvas.height = THREE.Math.ceilPowerOfTwo(this.parameters.size);
+        context.font = this.parameters.size + 'px ' + this.parameters.font;
+        let textWidth = context.measureText(this.parameters.text).width;
         this.canvas.width = THREE.Math.ceilPowerOfTwo(textWidth);
-        context.font = parameters.size + 'px ' + parameters.font;
+        context.font = this.parameters.size + 'px ' + this.parameters.font;
         context.textAlign = 'center';
-        context.fillStyle = parameters.color;
-        context.fillText(parameters.text, this.canvas.width / 2, parameters.size);
+        context.fillStyle = this.parameters.color;
+        context.fillText(this.parameters.text, this.canvas.width / 2, this.parameters.size);
         let texture = new THREE.CanvasTexture(this.canvas);
         texture.needsUpdate = true;
         let material = new THREE.SpriteMaterial({map: texture});
@@ -30,8 +34,21 @@ export default class VisualLabel extends VisualModelAbstract {
 
     render(epoch) {
         this.threeObj.position.copy(sim.getVisualCoords(this.functionOfEpoch.evaluate(epoch)));
-        this.scaleKoeff = this.threeObj.position.length() * sim.raycaster.getPixelAngleSize();
+        this.scaleKoeff = this.calculateScaleKoeff(this.threeObj.position, this.parameters.scalingRange);
         this.sprite.scale.x = this.canvas.width  * this.scaleKoeff;
         this.sprite.scale.y = this.canvas.height * this.scaleKoeff;
+    }
+
+    calculateScaleKoeff(position, range) {
+        if (typeof range.callback !== 'undefined') {
+            return range.callback(position, range);
+        }
+        if (position.length() < range.from){
+            return range.from * sim.raycaster.getPixelAngleSize();
+        }
+        if (position.length() > range.to) {
+            return range.to * sim.raycaster.getPixelAngleSize();
+        }
+        return position.length() * sim.raycaster.getPixelAngleSize();
     }
 }
