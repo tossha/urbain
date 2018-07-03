1a50a3fe268a59dfac0b9c733bc49dea7d998139
ronatskiy <r.onatskiy@gmail.com>
2018-07-03 22:24:33 +0300

    Remove redundant method. Used bundle hash instead of set file creation time

--- public/functions.php
+++ public/functions.php
@@ -1,9 +1,5 @@
 <?php
 
-function addTime($file) {
-    return $file . '?t=' . filemtime($file);
-}
-
 function generateToggleButton($name) {
     return '<button id="' . $name . 'ToggleButton" onclick="sim.ui.changeVisibility(\''
         . $name . '\')" class="toggleButton">Hide</button>';
