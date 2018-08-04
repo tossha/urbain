b91f85032f53426584f440d7a14c979eab9f61a8
ronatskiy <r.onatskiy@gmail.com>
2018-08-04 16:28:52 +0300

    Added icon on camera panel

--- src/components/main-view/components/bottom-panel/camera-panel.js
+++ src/components/main-view/components/bottom-panel/camera-panel.js
@@ -1,23 +1,25 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import "./camera-panel.css";
 import Panel from "../../../common/panel";
 
-const CameraPanel = ({ className }) => {
+const CameraPanel = () => {
     return (
-        <div className={className} id="cameraPanel">
-            <Panel className="camera-panel" caption="Camera">
-                <Fragment>
-                    <label className="camera-panel__field">
-                        Target: <select className="camera-panel__field-control" id="targetSelect" />
-                    </label>
-                    <label className="camera-panel__field">
-                        Frame type: <select className="camera-panel__field-control" id="rfTypeSelect" />
-                    </label>
-                </Fragment>
-            </Panel>
-        </div>
+        <Panel
+            id="cameraPanel"
+            className="camera-panel"
+            caption="Camera"
+            titleIcon={<FontAwesomeIcon className="camera-panel__icon" icon="expand" />}
+        >
+            <label className="camera-panel__field">
+                Target: <select className="camera-panel__field-control" id="targetSelect" />
+            </label>
+            <label className="camera-panel__field">
+                Frame type: <select className="camera-panel__field-control" id="rfTypeSelect" />
+            </label>
+        </Panel>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faExpand } from "@fortawesome/free-solid-svg-icons"
 import { init } from "./main";
 
 import AppComponent from "./components/app";
+
+library.add(faExpand);
 
 init();
 
