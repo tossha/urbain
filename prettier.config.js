63bcfb0b5f7b840891ff1c21e2904d120cf6de61
ronatskiy <r.onatskiy@gmail.com>
2018-08-10 18:40:16 +0300

    Added prettier format checking tool

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+};
