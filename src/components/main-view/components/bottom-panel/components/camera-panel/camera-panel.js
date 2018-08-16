889c0f71e4b3033f78b8836de8aef4147841c26f
ronatskiy <r.onatskiy@gmail.com>
2018-08-16 20:30:48 +0300

    Refactored camera panel

--- src/components/main-view/components/bottom-panel/components/camera-panel/camera-panel.js
+++ src/components/main-view/components/bottom-panel/components/camera-panel/camera-panel.js
@@ -1,32 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./camera-panel.css";
 import Panel from "../../../../../common/panel";
 
-const CameraPanel = () => {
-    return (
-        <Panel
-            id="cameraPanel"
-            className="camera-panel"
-            caption="Camera"
-            titleIcon={<FontAwesomeIcon className="camera-panel__icon" icon="expand" />}
-        >
-            <div className="camera-panel__content">
-                <label className="panel__field camera-panel__field">
-                    Target: <select className="camera-panel__field-control" id="targetSelect" />
-                </label>
-                <label className="panel__field camera-panel__field">
-                    Frame type: <select className="camera-panel__field-control" id="rfTypeSelect" />
-                </label>
-            </div>
-        </Panel>
-    );
-};
-
-CameraPanel.propTypes = {
-    className: PropTypes.string,
-};
+const CameraPanel = () => (
+    <Panel id="cameraPanel" className="camera-panel" caption="Camera" titleIcon={<FontAwesomeIcon icon="expand" />}>
+        <div className="camera-panel__content">
+            <label className="panel__field camera-panel__field">
+                <span className="panel__field-label">Target:</span>
+                <select className="camera-panel__field-control" id="targetSelect" />
+            </label>
+            <label className="panel__field camera-panel__field">
+                <span className="panel__field-label">Frame type:</span>
+                <select className="camera-panel__field-control" id="rfTypeSelect" />
+            </label>
+        </div>
+    </Panel>
+);
 
 export default CameraPanel;
