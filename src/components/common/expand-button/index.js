afea9b732ef52e86e81b9a9dc179ef1614a300e5
ronatskiy <r.onatskiy@gmail.com>
2018-08-16 20:23:04 +0300

    Implemented ExpandButton component

---
+++ src/components/common/expand-button/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./index.css";
+
+function ExpandButton({ className, onClick, expanded = false }) {
+    return (
+        <button
+            type="button"
+            className={cn(className, "expand-button", {
+                "expand-button--expanded": expanded,
+            })}
+            onClick={onClick}
+        >
+            <FontAwesomeIcon className="expand-button__icon" icon="chevron-down" />
+        </button>
+    );
+}
+
+ExpandButton.propTypes = {
+    className: PropTypes.string,
+    expanded: PropTypes.bool,
+    onClick: PropTypes.func,
+};
+
+export default ExpandButton;
