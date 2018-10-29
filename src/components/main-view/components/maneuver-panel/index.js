cc4ff07b1cb0b619c99259c0c7b9f9563b1d48aa
Roman Onatskiy <r.onatskiy@gmail.com>
2018-10-29 18:52:26 +0200

    Clean up maneuver-panel

--- src/components/main-view/components/maneuver-panel/index.js
+++ src/components/main-view/components/maneuver-panel/index.js
@@ -1,22 +1,17 @@
 import React from "react";
 
 import Panel from "../../../common/panel";
-import "./index.css";
+import "./index.scss";
 
 const ManeuverPanel = ({ className }) => (
-    <Panel
-        id="maneuverPanel"
-        className={className}
-        caption="Maneuver"
-        collapseDirection="left"
-    >
+    <Panel id="maneuverPanel" className={className} caption="Maneuver" collapseDirection="left">
         <div className="metrics-panel__content">
             <div className="panel__field-set">
                 <label className="panel__field panel__field--centered">
                     <button className="maneuver-panel__button" type="button" id="prev-burn">
                         Prev burn
                     </button>
-                    <span className="panel__field-label" id="burn-name"></span>
+                    <span className="panel__field-label" id="burn-name" />
                     <button className="maneuver-panel__button" type="button" id="next-burn">
                         Next burn
                     </button>
@@ -26,14 +21,26 @@ const ManeuverPanel = ({ className }) => (
             <div className="panel__field-set">
                 <div className="panel__field panel__field-header">Increment</div>
                 <div className="panel__field">
-                    <span className="panel__field-label-small" id="increment"></span>
+                    <span className="panel__field-label-small" id="increment" />
                     <div className="maneuver-panel__button-block">
-                        <button className="maneuver-panel__button" type="button" id="incr001">0.01</button>
-                        <button className="maneuver-panel__button" type="button" id="incr01">0.1</button>
-                        <button className="maneuver-panel__button" type="button" id="incr1">1</button>
-                        <button className="maneuver-panel__button" type="button" id="incr10">10</button>
-                        <button className="maneuver-panel__button" type="button" id="incr100">100</button>
-                        <button className="maneuver-panel__button" type="button" id="incr1000">1000</button>
+                        <button className="maneuver-panel__button" type="button" id="incr001">
+                            0.01
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="incr01">
+                            0.1
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="incr1">
+                            1
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="incr10">
+                            10
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="incr100">
+                            100
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="incr1000">
+                            1000
+                        </button>
                     </div>
                 </div>
             </div>
@@ -48,8 +55,12 @@ const ManeuverPanel = ({ className }) => (
                     <span className="panel__field-label">Epoch</span>
                     <span className="panel__field-control" id="burn-epoch" />
                     <div className="maneuver-panel__button-block">
-                        <button className="maneuver-panel__button" type="button" id="epoch-minus">-</button>
-                        <button className="maneuver-panel__button" type="button" id="epoch-plus">+</button>
+                        <button className="maneuver-panel__button" type="button" id="epoch-minus">
+                            -
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="epoch-plus">
+                            +
+                        </button>
                     </div>
                 </div>
             </div>
@@ -61,9 +72,15 @@ const ManeuverPanel = ({ className }) => (
                     <span className="panel__field-control" id="burn-prograde" />
                     <span className="metrics-panel__dimension">m/s</span>
                     <div className="maneuver-panel__button-block">
-                        <button className="maneuver-panel__button" type="button" id="prograde-zero">0</button>
-                        <button className="maneuver-panel__button" type="button" id="prograde-minus">-</button>
-                        <button className="maneuver-panel__button" type="button" id="prograde-plus">+</button>
+                        <button className="maneuver-panel__button" type="button" id="prograde-zero">
+                            0
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="prograde-minus">
+                            -
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="prograde-plus">
+                            +
+                        </button>
                     </div>
                 </div>
                 <div className="panel__field">
@@ -71,9 +88,15 @@ const ManeuverPanel = ({ className }) => (
                     <span className="panel__field-control" id="burn-normal" />
                     <span className="metrics-panel__dimension">m/s</span>
                     <div className="maneuver-panel__button-block">
-                        <button className="maneuver-panel__button" type="button" id="normal-zero">0</button>
-                        <button className="maneuver-panel__button" type="button" id="normal-minus">-</button>
-                        <button className="maneuver-panel__button" type="button" id="normal-plus">+</button>
+                        <button className="maneuver-panel__button" type="button" id="normal-zero">
+                            0
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="normal-minus">
+                            -
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="normal-plus">
+                            +
+                        </button>
                     </div>
                 </div>
                 <div className="panel__field">
@@ -81,9 +104,15 @@ const ManeuverPanel = ({ className }) => (
                     <span className="panel__field-control" id="burn-radial" />
                     <span className="metrics-panel__dimension">m/s</span>
                     <div className="maneuver-panel__button-block">
-                        <button className="maneuver-panel__button" type="button" id="radial-zero">0</button>
-                        <button className="maneuver-panel__button" type="button" id="radial-minus">-</button>
-                        <button className="maneuver-panel__button" type="button" id="radial-plus">+</button>
+                        <button className="maneuver-panel__button" type="button" id="radial-zero">
+                            0
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="radial-minus">
+                            -
+                        </button>
+                        <button className="maneuver-panel__button" type="button" id="radial-plus">
+                            +
+                        </button>
                     </div>
                 </div>
                 <div className="panel__field">
@@ -92,7 +121,6 @@ const ManeuverPanel = ({ className }) => (
                     <span className="metrics-panel__dimension">m/s</span>
                 </div>
             </div>
-
         </div>
     </Panel>
 );
