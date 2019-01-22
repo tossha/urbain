aeb074ab676f4a8f2ea5b50585bdcc1e5aebaab8
Roman Onatskiy <r.onatskiy@gmail.com>
2019-01-22 21:31:51 +0200

    Refactor select component

---
+++ src/ui/components/common/logos/dropdown-icon.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ReactComponent as ChevronDown } from "../images/chevron-down.svg";
+
+const DropdownIcon = ({ className }) => <ChevronDown className={`dropdown-icon ${className}`} />;
+
+export default DropdownIcon;
