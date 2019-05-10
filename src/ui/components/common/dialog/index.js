810db4d7387a39cf82554dea3b2b3360ce0c4cb5
Roman Onatskiy <r.onatskiy@gmail.com>
2019-05-10 16:08:11 +0300

    Made dialog draggable

--- src/ui/components/common/dialog/index.js
+++ src/ui/components/common/dialog/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
+import Draggable from "react-draggable";
 
 import { ReactComponent as CloseIcon } from "../images/cross.svg";
 import "./index.scss";
@@ -9,12 +10,15 @@ class Dialog extends React.Component {
     static propTypes = {
         className: PropTypes.string,
         caption: PropTypes.string,
+        draggable: PropTypes.bool,
         onClose: PropTypes.func,
         isOpen: PropTypes.bool,
+        children: PropTypes.node,
     };
 
     static defaultProps = {
         caption: "Dialog",
+        draggable: false,
         onClose() {},
         isOpen: false,
     };
@@ -24,25 +28,28 @@ class Dialog extends React.Component {
     };
 
     render() {
-        const { className, caption, isOpen, children } = this.props;
+        const { className, caption, isOpen, children, draggable } = this.props;
         const classNames = cn(
             "dialog",
             {
                 "dialog--open": isOpen,
+                "dialog--draggable": draggable,
             },
             className,
         );
 
         return (
-            <div className={classNames}>
-                <div className="dialog__header">
-                    <div className="dialog__caption">{caption}</div>
-                    <div className="dialog__close-button" onClick={this._handleClose}>
-                        <CloseIcon className="dialog__close-button-icon" />
+            <Draggable handle=".dialog__header" disabled={!draggable}>
+                <div className={classNames}>
+                    <div className="dialog__header">
+                        <div className="dialog__caption">{caption}</div>
+                        <div className="dialog__close-button" onClick={this._handleClose}>
+                            <CloseIcon className="dialog__close-button-icon" />
+                        </div>
                     </div>
+                    <div className="dialog__body">{children}</div>
                 </div>
-                <div className="dialog__body">{children}</div>
-            </div>
+            </Draggable>
         );
     }
 }
